bad455067377c44e35abe536117771081c744f99
dkman94 <deepak.s.kumar.85@gmail.com>
2022-02-02 18:15:27 -0800

    Initial Commit

---
+++ ios_take_home/ios_take_homeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ios_take_homeApp.swift
+//  ios_take_home
+//
+//  Created by Deepak Kumar on 2/2/22.
+//
+
+import SwiftUI
+
+@main
+struct ios_take_homeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
